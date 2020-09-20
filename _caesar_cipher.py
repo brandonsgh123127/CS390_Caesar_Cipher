0f06bc03cbfed6b606910dd285f07ab407aa046f
Brandon Spada <brandonsgh123127@gmail.com>
2020-09-20 01:21:46 -0400

    add except term for cipherkey

--- _caesar_cipher.py
+++ _caesar_cipher.py
@@ -1,9 +1,14 @@
+import sys
 def main():
     in_txt = str(input('Please input text to cipher:'))
     out_txt = []
     print('Input: ',in_txt)
-    _cipherkey = int(input('Please input cipher key (0-26)')) % 26
-    print('Key: ',_cipherkey)
+    try:
+        _cipherkey = int(input('Please input cipher key (-25 - 25)')) % 26
+    except:
+        print('Unknown value, exiting...')
+        sys.exit(-1)
+    print('Key: ', _cipherkey)
     for i in in_txt:
         out_txt.append(chr(ord(i)+_cipherkey))
     print(''.join(map(str,out_txt)))
