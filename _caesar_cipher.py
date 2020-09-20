b25943c1cc61f91cb6783e2d8f0a56155f584db8
Brandon Spada <brandonsgh123127@gmail.com>
2020-09-20 01:25:04 -0400

    Fix mod calc

--- _caesar_cipher.py
+++ _caesar_cipher.py
@@ -4,7 +4,8 @@ def main():
     out_txt = []
     print('Input: ',in_txt)
     try:
-        _cipherkey = int(input('Please input cipher key (-25 - 25)')) % 26
+        _cipherkey = int(input('Please input cipher key (-25 - 25)'))
+        _cipherkey = _cipherkey % 26 if _cipherkey > 0 else _cipherkey % -26
     except:
         print('Unknown value, exiting...')
         sys.exit(-1)
