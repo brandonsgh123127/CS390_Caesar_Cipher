c1d8c4d13583f8da3b23f6064675db2e606a52c8
Brandon Spada <brandonsgh123127@gmail.com>
2020-09-21 16:35:36 -0400

    Add decryption

--- _caesar_cipher.py
+++ _caesar_cipher.py
@@ -1,11 +1,50 @@
 import sys
+
 '''
 Caesar Cipher Implementation
 Author: Brandon Spada & Leonardo Pegeas
 '''
-def main():
-    in_txt = str(input('Please input text to cipher: '))
+
+
+def encryption(in_txt, _cipherkey):
     out_txt = []
+    for i in in_txt:
+        # Space stays the same
+        if ord(i) == 32:
+            out_txt.append(chr(ord(i)))
+        # Lower Case letter boundaries
+        elif 97 <= ord(i) <= 122:
+            out_txt.append(chr((ord(i) + _cipherkey - 97) % 26 + 97))
+        # Upper case letter boundaries
+        elif 65 <= ord(i) <= 90:
+            out_txt.append(chr((ord(i) + _cipherkey - 65) % 26 + 65))
+        else:
+            out_txt.append(chr(ord(i) + _cipherkey))
+
+    print('Encrypted: ', ''.join(map(str, out_txt)))
+    return ''.join(map(str, out_txt))
+
+
+def decryption(in_txt, _cipherkey):
+    out_txt = []
+    for i in in_txt:
+        # Space stays the same
+        if ord(i) == 32:
+            out_txt.append(chr(ord(i)))
+        # Lower Case letter boundaries
+        elif 97 <= ord(i) <= 122:
+            out_txt.append(chr((ord(i) - _cipherkey - 97) % 26 + 97))
+        # Upper case letter boundaries
+        elif 65 <= ord(i) <= 90:
+            out_txt.append(chr((ord(i) - _cipherkey - 65) % 26 + 65))
+        else:
+            out_txt.append(chr(ord(i) - _cipherkey))
+    print('Decrypted: ', ''.join(map(str, out_txt)))
+
+
+def main():
+    e_txt = ""
+    in_txt = str(input('Please input text to cipher: '))
     print('Input: ', in_txt)
     # Verify key is correct
     try:
@@ -19,24 +58,8 @@ def main():
     For each letter in the input text, shift ascii number by value of cipher key,
     then append to a list that will contain new values.
     '''
-    for i in in_txt:
-        # Space stays the same
-        if ord(i) == 32:
-            out_txt.append(chr(ord(i)))
-        # Lower Case letter boundaries
-        elif ord(i) + _cipherkey < 97 and 97 <= ord(i) <= 122:
-            out_txt.append(chr(ord(i) + _cipherkey + 26))
-        elif ord(i) + _cipherkey > 122 and 97 <= ord(i) <= 122:
-            out_txt.append(chr(ord(i) + _cipherkey - 26))
-        # Upper case letter boundaries
-        elif ord(i) + _cipherkey > 90 and 65 <= ord(i) <= 90:
-            out_txt.append(chr(ord(i) + _cipherkey + 26))
-        elif ord(i) + _cipherkey < 65 and 65 <= ord(i) <= 90:
-            out_txt.append(chr(ord(i) + _cipherkey + 26))
-        else:
-            out_txt.append(chr(ord(i) + _cipherkey))
-    print('Encrypted: ', ''.join(map(str, out_txt)))
-
+    e_txt = encryption(in_txt, _cipherkey)
+    decryption(e_txt, _cipherkey)
     '''
     Decryption phase: TO BE IMPLEMENTED:
                         We have encryption variable key, just get opposite value of key and apply another loop
